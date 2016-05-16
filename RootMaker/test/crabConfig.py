ef68e20326a2d0f823ed570822b1bed91c87d383
E Kennedy <ekennedy@cern.ch>
2016-05-16 14:01:15 +0200

    crab config

--- RootMaker/test/crabConfig.py
+++ RootMaker/test/crabConfig.py
@@ -2,24 +2,29 @@ from CRABClient.UserUtilities import config
 config = config()
 
 # taskname
-config.General.requestName = 'newrootmakertest_try1'
-#config.General.workArea = ''
+config.General.requestName = '_try1'
 
 config.JobType.pluginName = 'Analysis'
 
 config.JobType.psetName = 'RootTree.py'
 
 # input and JSON
-config.Data.inputDataset = '/SingleMuon/Run2015C_25ns-16Dec2015-v1/MINIAOD'
-config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Reprocessing/Cert_13TeV_16Dec2015ReReco_Collisions15_25ns_JSON_v2.txt'
+config.Data.inputDataset = ''
+config.Data.lumiMask = ''
 
 # job splitting options
 config.Data.inputDBS = 'global'
-config.Data.splitting = 'LumiBased'
-config.Data.unitsPerJob = 3
+
+# for data:
+#config.Data.splitting = 'LumiBased'
+#config.Data.unitsPerJob = 3
+
+# for MC:
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
 
 # storage options
-config.Data.outLFNDirBase = '/store/user/ekennedy/smh2mu/data/test'
+config.Data.outLFNDirBase = '/store/user/ekennedy/smh2mu/'
 config.Site.storageSite = 'T2_CH_CERN'
 
 config.Data.publication = False
