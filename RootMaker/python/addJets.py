f769a5a66bf3f23a0fdceded6d663ee1825bd5a3
E Kennedy <ekennedy@cern.ch>
2017-07-17 05:05:55 +0200

    l re for data

--- RootMaker/python/addJets.py
+++ RootMaker/python/addJets.py
@@ -17,6 +17,10 @@ jetBranches = commonJetTauBranches.clone(
     btag_passCMVAv2M = cms.vstring('? bDiscriminator("pfCombinedMVAV2BJetTags") > 0.4432 ? 1 : 0','I'),
     btag_passCMVAv2T = cms.vstring('? bDiscriminator("pfCombinedMVAV2BJetTags") > 0.9432 ? 1 : 0','I'),
 
+    # user data embedded from JetShiftEmbedder
+    #pt_Down = cms.vstring('userFloat("pt_Down")','F'),
+    #pt_Up   = cms.vstring('userFloat("pt_Up")','F'),
+
     # user data embedded from JetIDEmbedder
     is_loose        = cms.vstring('userInt("idLoose")','I'),
     is_tight        = cms.vstring('userInt("idTight")','I'),
@@ -27,11 +31,11 @@ jetBranches = commonJetTauBranches.clone(
     #puid_medium     = cms.vstring('userInt("puid_medium")','I'),
     #puid_tight      = cms.vstring('userInt("puid_tight")','I'),
 
-    # energy shifts
-    pt_jetEnUp       = cms.vstring('? hasUserCand("JetEnUp") ? userCand("JetEnUp").pt() : 0','F'),
-    energy_jetEnUp   = cms.vstring('? hasUserCand("JetEnUp") ? userCand("JetEnUp").energy() : 0','F'),
-    pt_jetEnDown     = cms.vstring('? hasUserCand("JetEnDown") ? userCand("JetEnDown").pt() : 0','F'),
-    energy_jetEnDown = cms.vstring('? hasUserCand("JetEnDown") ? userCand("JetEnDown").energy() : 0 ','F'),
+    ## energy shifts
+    #pt_jetEnUp       = cms.vstring('? hasUserCand("JetEnUp") ? userCand("JetEnUp").pt() : 0','F'),
+    #energy_jetEnUp   = cms.vstring('? hasUserCand("JetEnUp") ? userCand("JetEnUp").energy() : 0','F'),
+    #pt_jetEnDown     = cms.vstring('? hasUserCand("JetEnDown") ? userCand("JetEnDown").pt() : 0','F'),
+    #energy_jetEnDown = cms.vstring('? hasUserCand("JetEnDown") ? userCand("JetEnDown").energy() : 0 ','F'),
 
 )
 
@@ -41,6 +45,7 @@ jetBranches = commonJetTauBranches.clone(
 ################################################
 def addJets(process, coll, **kwargs):
 # note: jet cleaning is defined in RootTree.py
+    runP = kwargs.pop('runP', 'NA')
     isMC = kwargs.pop('isMC', False)
     jSrc = coll['ak4pfchsjets']
     pvSrc = coll['vertices']
@@ -49,27 +54,20 @@ def addJets(process, coll, **kwargs):
     # customization path
     process.jetCustomization = cms.Path()
 
-    # recorrect jets
-    ######################
-    #####from PhysicsTools.PatAlgos.tools.jetTools import updateJetCollection
 
-    #####jetCorr = ('AK4PFchs', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute', 'L2L3Residual']), 'None')
-    #####if isMC:
-    #####    jetCorr = ('AK4PFchs', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute']), 'None')
-    #####updateJetCollection(
-    #####    process,
-    #####    jetSource = cms.InputTag(jSrc),
-    #####    jetCorrections = jetCorr,
-    #####)
-    #####process.updatedPatJets.userData.userFloats.src += ['pileupJetIdUpdated:fullDiscriminant']
-    #####jSrc = 'updatedPatJets'
+    ####################
+    #### recorrect jets
+    ####################
+    levels = ['L1FastJet',
+              'L2Relative',
+              'L3Absolute']
+    if not isMC: levels.append('L2L3Residual')
+
 
     from PhysicsTools.PatAlgos.producersLayer1.jetUpdater_cff import updatedPatJetCorrFactors
     process.patJetCorrFactorsReapplyJEC = updatedPatJetCorrFactors.clone(
         src = cms.InputTag(jSrc),
-        levels = ['L1FastJet', 
-                  'L2Relative', 
-                  'L3Absolute'],
+        levels = levels,
         payload = 'AK4PFchs' 
     )
     from PhysicsTools.PatAlgos.producersLayer1.jetUpdater_cff import updatedPatJets
@@ -88,6 +86,27 @@ def addJets(process, coll, **kwargs):
     )
     process.jetCustomization *= process.jID
     jSrc = 'jID'
+
+    ## embed pt shifts
+    #jecUncDataFile_ = 'RootMaker/RootMaker/data/'
+    #if isMC:
+    #    jecUncDataFile_ += 'Summer16_23Sep2016V4_MC_Uncertainty_AK4PFchs.txt'
+    #elif runP in ['B','C','D']:
+    #    jecUncDataFile_ += 'Summer16_23Sep2016BCDV4_DATA_Uncertainty_AK4PFchs.txt'
+    #elif runP in ['E','F']:
+    #    jecUncDataFile_ += 'Summer16_23Sep2016EFV4_DATA_Uncertainty_AK4PFchs.txt'
+    #elif runP in ['G']:
+    #    jecUncDataFile_ += 'Summer16_23Sep2016GV4_DATA_Uncertainty_AK4PFchs.txt'
+    #else:
+    #    jecUncDataFile_ += 'Summer16_23Sep2016HV4_DATA_Uncertainty_AK4PFchs.txt'
+
+    #process.jShift = cms.EDProducer(
+    #    'JetShiftEmbedder',
+    #    src = cms.InputTag(jSrc),
+    #    jecUncDataFile = cms.FileInPath(jecUncDataFile_),
+    #)
+    #jSrc = 'jShift'
+    #process.jetCustomization *= process.jShift
 
     #process.load('RecoJets.JetProducers.PileupJetID_cfi')
     #process.jpuID = process.pileupJetId.clone(
